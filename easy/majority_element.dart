fcbfba65b2b5e5b2ef603cb55f5679170734254c
eylulkaratas <eylulkaratas22@gmail.com>
2026-03-01 01:28:54 +0300

    Create majority_element.dart

---
+++ easy/majority_element.dart
@@ -0,0 +1,14 @@
+class Solution {
+  int majorityElement(List<int> nums) {
+    Map<int, int> count = {};
+
+    for (int num in nums) {
+      count[num] = (count[num] ?? 0) + 1;
+      if (count[num]! > nums.length ~/ 2) {
+        return num;
+      }
+    }
+
+    return -1;
+  }
+}
