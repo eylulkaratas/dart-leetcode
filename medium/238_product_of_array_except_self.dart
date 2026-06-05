5d5706a373cdac7ad0d573515bff2722388cfff4
eylulkaratas <eylulkaratas22@gmail.com>
2026-06-05 14:54:55 +0300

    Create 238_product_of_array_except_self.dart

---
+++ medium/238_product_of_array_except_self.dart
@@ -0,0 +1,14 @@
+List<int> productExceptSelf(List<int> nums) {
+  int length = nums.length;
+  List<int> answer = List.filled(length, 0);
+  answer[0] = 1;
+  for (int i = 1; i < length; i++) {
+    answer[i] = nums[i - 1] * answer[i - 1];
+  }
+  int R = 1;
+  for (int i = length - 1; i >= 0; i--) {
+    answer[i] = answer[i] * R;
+    R *= nums[i];
+  }
+  return answer;
+}
