e65640dcd69e7cf16618b603deb5efa20def8dd6
eylulkaratas <eylulkaratas22@gmail.com>
2026-06-05 14:45:30 +0300

    Create 206_reverse_linked_list.dart

---
+++ easy/206_reverse_linked_list.dart
@@ -0,0 +1,17 @@
+class ListNode {
+  int val;
+  ListNode? next;
+  ListNode([this.val = 0, this.next]);
+}
+
+ListNode? reverseList(ListNode? head) {
+  ListNode? prev = null;
+  ListNode? curr = head;
+  while (curr != null) {
+    ListNode? nextTemp = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = nextTemp;
+  }
+  return prev;
+}
