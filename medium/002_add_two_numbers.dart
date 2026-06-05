276e5b8b04c979357e475d399cc2d9790946da89
eylulkaratas <eylulkaratas22@gmail.com>
2026-06-05 14:51:50 +0300

    Create 002_add_two_numbers.dart

---
+++ medium/002_add_two_numbers.dart
@@ -0,0 +1,16 @@
+ListNode? addTwoNumbers(ListNode? l1, ListNode? l2) {
+  ListNode dummyHead = ListNode(0);
+  ListNode? curr = dummyHead;
+  int carry = 0;
+  while (l1 != null || l2 != null || carry != 0) {
+    int x = (l1 != null) ? l1.val : 0;
+    int y = (l2 != null) ? l2.val : 0;
+    int sum = carry + x + y;
+    carry = sum ~/ 10;
+    curr?.next = ListNode(sum % 10);
+    curr = curr?.next;
+    if (l1 != null) l1 = l1.next;
+    if (l2 != null) l2 = l2.next;
+  }
+  return dummyHead.next;
+}
