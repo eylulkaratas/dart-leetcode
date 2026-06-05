2091da81fd7c1fb1dbb721f7373d046d91865e4b
eylulkaratas <eylulkaratas22@gmail.com>
2026-06-05 14:40:45 +0300

    Create 190_reverse_bits.dart

---
+++ easy/190_reverse_bits.dart
@@ -0,0 +1,8 @@
+int reverseBits(int n) {
+  int result = 0;
+  for (int i = 0; i < 32; i++) {
+    result = (result << 1) | (n & 1);
+    n >>= 1;
+  }
+  return result & 0xFFFFFFFF; // 32-bit unsigned tam sayı maskesi
+}
