3d9664f0dc27dda48e44b27748aa531aa6eb5120
eylulkaratas <eylulkaratas22@gmail.com>
2026-06-05 14:44:42 +0300

    Create 167_two_sum_ii_input_array_is_sorted.dart

---
+++ easy/167_two_sum_ii_input_array_is_sorted.dart
@@ -0,0 +1,15 @@
+List<int> twoSum(List<int> numbers, int target) {
+  int low = 0;
+  int high = numbers.length - 1;
+  while (low < high) {
+    int sum = numbers[low] + numbers[high];
+    if (sum == target) {
+      return [low + 1, high + 1];
+    } else if (sum < target) {
+      low++;
+    } else {
+      high--;
+    }
+  }
+  return [-1, -1];
+}
