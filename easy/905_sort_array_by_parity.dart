bfc77c2c8f0521735eb6df9251dd738f5a55111f
eylulkaratas <eylulkaratas22@gmail.com>
2026-06-05 14:47:01 +0300

    Create 905_sort_array_by_parity.dart

---
+++ easy/905_sort_array_by_parity.dart
@@ -0,0 +1,13 @@
+List<int> sortArrayByParity(List<int> nums) {
+  int i = 0, j = nums.length - 1;
+  while (i < j) {
+    if (nums[i] % 2 > nums[j] % 2) {
+      int temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
+    }
+    if (nums[i] % 2 == 0) i++;
+    if (nums[j] % 2 != 0) j--;
+  }
+  return nums;
+}
