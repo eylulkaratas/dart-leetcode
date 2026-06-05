9df65f1535f70dc70515aba7b9de0c4ff39ba820
eylulkaratas <eylulkaratas22@gmail.com>
2026-06-05 14:40:20 +0300

    Create 119_pascals_triangle_ii.dart

---
+++ easy/119_pascals_triangle_ii.dart
@@ -0,0 +1,9 @@
+List<int> getRow(int rowIndex) {
+  List<int> row = List.filled(rowIndex + 1, 1);
+  for (int i = 1; i < rowIndex; i++) {
+    for (int j = i; j > 0; j--) {
+      row[j] += row[j - 1];
+    }
+  }
+  return row;
+}
